11e5557f2ba6361951c72a0dbdbc5b9f95e8f3aa
project0su <119090009+project0su@users.noreply.github.com>
2022-11-27 18:44:30 -0300

    Agrego cambios

--- src/app/input-integer/input-integer.component.ts
+++ src/app/input-integer/input-integer.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-input-integer',
@@ -11,18 +11,29 @@ export class InputIntegerComponent implements OnInit {
 
   @Input() 
   quantity: number;
+
+  @Input()
   max: number;
+
+  @Output()
+  quantityChange: EventEmitter<number>= new EventEmitter<number>();
 
   ngOnInit(): void {}
 
   upQuantity(): void {
-    if (this.quantity < this.max) this.quantity++;
+    if (this.quantity < this.max) {
+      this.quantity++;
+      this.quantityChange.emit(this.quantity);
+    }
   }
 
   downQuantity(): void {
-    if (this.quantity > 0) this.quantity--;
+    if (this.quantity > 0) {
+      this.quantity--;
+      this.quantityChange.emit(this.quantity);
+    }
   }
-  changeQuantity(event, ): void {
+  changeQuantity(event): void {
     console.log(event.key);
   }
 }
