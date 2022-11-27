81e19c3199b253e92ed3c95884f5772192a39267
project0su <119090009+project0su@users.noreply.github.com>
2022-11-27 14:31:59 -0300

    Agrego cambios

---
+++ src/app/AppleShop-apples/AppleShop-apples.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-AppleShop-apples',
+  templateUrl: './AppleShop-apples.component.html',
+  styleUrls: ['./AppleShop-apples.component.scss'],
+})
+export class AppleShopComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,20 @@
-import { BreweryBeersComponent } from './brewery-beers/brewery-beers.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { AppleShopComponent } from './AppleShop-apples/AppleShop-apples.component';
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'beers',
+    redirectTo: 'apples',
     pathMatch: 'full',
   },
   {
-    path: 'beers',
+    path: 'apples',
+    component: AppleShopComponent,
+  },
+  {
+    path: 'about',
     component: AppComponent,
   },
 ];
