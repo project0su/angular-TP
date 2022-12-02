f38652ba7545213e891d1f68b28399431a848b5d
project0su <119090009+project0su@users.noreply.github.com>
2022-12-02 00:42:35 -0300

    Agrego cambios.

---
+++ src/app/apple-favorites.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Apple } from './apple-list/Apple';
+
+/**
+ * logica del carrito
+ * 
+ */
+@Injectable({
+  providedIn: 'root'
+})
+export class AppleFavoritesService {
+
+  private _favoritesList: Apple[] = [];
+
+  favoritesList: BehaviorSubject<Apple[]> = new BehaviorSubject([]);
+
+  constructor() { }
+
+  addToFavorites(apple: Apple) {
+    let item: Apple = this._favoritesList.find((v1) => v1.name == apple.name);
+    if(!item) {
+      this._favoritesList.push({ ... apple});
+      this.favoritesList.next(this._favoritesList); // equivalente al emitt de eventos
+    }
+  }
+
+  removeFromFavorites(apple: Apple) {
+    /* let item: Apple = this._crateList.find((v1) => v1.name == apple.name); */
+    let i: number= 0;
+    while (this._favoritesList[i] != null && (this._favoritesList[i].name != apple.name)) {
+      i++
+    }
+    let item= this._favoritesList[i];
+    if(item) {               //para sacarlo del arreglo
+      this._favoritesList.splice(i);
+      this.favoritesList.next(this._favoritesList); // equivalente al emitt de eventos
+    }
+  }
+}
--- src/app/apple-list/apple-list.component.ts
+++ src/app/apple-list/apple-list.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { fromEventPattern } from 'rxjs';
 import { AppleCrateService } from '../apple-crate.service';
+import { AppleFavoritesService } from '../apple-favorites.service';
 import { AppleDataService } from '../apple-data.service';
 import { Apple } from './Apple';
+
 
 @Component({
   selector: 'app-apple-list',
@@ -15,9 +17,10 @@ export class AppleListComponent implements OnInit {
     /* apples: Apple[]= [{"name":"Manzana 7","sign":"Indefinido","stock":4,"image":'assets/img/manzana.png',"new":false, quantity: 0},{"name":"Manzana 2","sign":"Indefinido", "stock":1,"image":"assets/img/manzana.png","new":false, quantity: 0}]; */
 
     
-      constructor(
-        private crate: AppleCrateService,
-        private AppleDataService: AppleDataService) {}
+    constructor(
+      private crate: AppleCrateService,
+      private AppleDataService: AppleDataService,
+      private favorites: AppleFavoritesService) {}
 
       ngOnInit(): void {
         this.AppleDataService.getAll()
@@ -31,11 +34,16 @@ export class AppleListComponent implements OnInit {
           apple.quantity= 0;
         }
       }
-
       removeFromCrate(apple): void {          
         this.crate.returnStock(apple);         //devuelve todo el stock consumido por el cajon al item apple
         this.crate.removeFromCrate(apple);      // para que no quede el elemento con valor 0 en el cajon
       }
 
+      addToFavorites(apple): void {
+        this.favorites.addToFavorites(apple);
+      }
+      removeFromFavorites(apple): void {      
+        this.favorites.removeFromFavorites(apple);      // para que no quede el elemento
+      }
 
 }
